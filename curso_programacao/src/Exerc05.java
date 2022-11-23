c6a0714e322293f8c02bbc61c6fb6189dadfe921
letnagao <leticia-nagao@hotmail.com>
2022-11-22 22:35:00 -0300

    finishing exercises 5 to 6

---
+++ curso_programacao/src/Exerc05.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exerc05 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int codigo1, codigo2, numeroPecas1, numeroPecas2;
+		double valorUnitario1, valorUnitario2, total1, total2, soma;
+		codigo1 = sc.nextInt();
+		numeroPecas1 = sc.nextInt();
+		valorUnitario1 = sc.nextDouble();
+		total1 = numeroPecas1 * valorUnitario1;
+		
+		codigo2 = sc.nextInt();
+		numeroPecas2 = sc.nextInt();
+		valorUnitario2 = sc.nextDouble();
+		total2 = numeroPecas2 * valorUnitario2;
+		
+		soma = total1 + total2;
+		
+		System.out.printf("VALOR A PAGAR: R$ %.2f", soma);
+		sc.close();
+	}
+
+}
---
+++ curso_programacao/src/Exerc06.java
@@ -0,0 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Exerc06 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C, triangulo, circulo, CQuadrado, trapezio, quadrado, retangulo;
+		
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		triangulo = A * C / 2;
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		
+		CQuadrado = Math.pow(C, 2);
+		circulo = 3.14159 * CQuadrado; 
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		
+		trapezio = (A + B) / 2.0 * C;
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		
+		quadrado = B * B;
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		
+		retangulo = A * B;
+		System.out.printf("RETANGULO: %.3f%n", retangulo);
+
+		sc.close();
+	}
+
+}
