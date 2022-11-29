bc72e3a514f84e8565034a3f01f76f37e2a716d6
letnagao <leticia-nagao@hotmail.com>
2022-11-28 21:18:41 -0300

    added repetitive structures

---
+++ curso_programacao/src/c_repetitive_structures/Exerc01.java
@@ -0,0 +1,21 @@
+package c_repetitive_structures;
+
+import java.util.Scanner;
+
+public class Exerc01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		sc.close();
+	}
+
+}
---
+++ curso_programacao/src/c_repetitive_structures/Exerc10.java
@@ -0,0 +1,21 @@
+package c_repetitive_structures;
+
+import java.util.Scanner;
+
+public class Exerc10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			int primeiro = i;
+			int segundo = i * i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+		}
+		
+		sc.close();
+	}
+}
